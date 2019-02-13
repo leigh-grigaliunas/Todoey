85808d877b78f476363a2af3916cb40bcc8fcbdf
Leigh Grigaliunas <leigh.grigaliunas@gmail.com>
2019-02-14 08:39:06 +1000

    Convert to MVC design pattern.

---
+++ Todoey/New Group/Item.swift
@@ -0,0 +1,21 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Leigh Grigaliunas on 13/2/19.
+//  Copyright © 2019 Diversity Digital. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+//    init(itemTitle:String, itemChecked: Bool) {
+//        title = itemTitle
+//        done = itemChecked
+//    }
+    
+}
